ade235125f3917ff4b72bb01e4e180837362567b
Hai Nguyen <zuzususu1999@gmail.com>
2025-07-13 12:36:18 +0700

    update calendar wedding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CalendarComponent } from './calendar/calendar.component';
 
 @Component({
@@ -12,4 +12,33 @@ import { CalendarComponent } from './calendar/calendar.component';
 })
 export class AppComponent {
   title = 'nhatvi-wedding';
+
+  animated = false;
+
+  toggleAnimation() {
+    this.animated = !this.animated;
+  }
+
+  // if croll to element with id 'wedding-images', toggle animation
+  @HostListener('window:scroll')
+  onScroll() {
+    if (!this.animated) {
+      this.checkIfInViewport();
+    }
+  }
+  
+
+  // if element with id 'wedding-images' is in viewport, toggle animation
+  checkIfInViewport() {
+    const element = document.getElementById('wedding-images');
+    if (element) {
+      const rect = element.getBoundingClientRect();
+      const isInViewport = rect.top >= 0 && rect.bottom <= window.innerHeight;
+      if (isInViewport) {
+        this.animated = true;
+      } else {
+        this.animated = false;
+      }
+    }
+  }
 }
--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import { NgClass, NgFor } from "@angular/common";
+import { NgClass } from "@angular/common";
 import { Component } from "@angular/core";
 
 
@@ -8,12 +8,12 @@ import { Component } from "@angular/core";
     styleUrls: ["./calendar.component.scss"],
     standalone: true,
     imports: [
-        NgFor,
         NgClass
     ]
 })
 export class CalendarComponent {
     // Add any properties or methods needed for the calendar functionality
-    days = Array.from({ length: 31 }, (_, i) => i + 1);
+    dateInWeek = ['T2', 'T3', 'T4', 'T5', 'T6', 'T7', 'CN'];
+    daysInMonth = ['', '', '', '', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
     highlightDay = 2;
 }
